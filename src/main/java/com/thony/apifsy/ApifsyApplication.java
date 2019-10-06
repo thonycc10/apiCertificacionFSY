9be5b6e78bd4f726b4db2bbd35d2adf6cb3989e9
Anthony Carrasco <anthony.carrasco@skf.com>
2019-10-06 00:16:00 -0500

    AC: First commit

---
+++ src/main/java/com/thony/apifsy/ApifsyApplication.java
@@ -0,0 +1,13 @@
+package com.thony.apifsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifsyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApifsyApplication.class, args);
+    }
+
+}
